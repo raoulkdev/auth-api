4d06a243ad02797ad0d5e5123205be847ff51bda
Raoul Kaleba <raouldev@outlook.com>
2025-07-14 21:15:06 -0500

    Added password and username length checking

--- src/handlers.rs
+++ src/handlers.rs
@@ -11,22 +11,28 @@ use crate::user::{User, UserPayload};
 
 // Add a new user
 pub async fn create_user(State(users_database): State<Arc<Pool<Postgres>>>, Json(payload): Json<UserPayload>) -> impl IntoResponse {
-    // Hash password
-    let salt = SaltString::generate(&mut OsRng);
-    let argon2 = Argon2::default();
-    let hashed_password = argon2.hash_password(payload.password.as_bytes(), &salt).unwrap().to_string();
+    // Check if passwords meet length requirements
+    if !payload.username.len() <= 3 || payload.password.len() < 8 {
+        // Hash password
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+        let hashed_password = argon2.hash_password(payload.password.as_bytes(), &salt).unwrap().to_string();
+        
+        // Add user
+        let new_user = sqlx::query_as::<_, User>("INSERT INTO users (username, hashed_password, last_login_at) VALUES ($1, $2, now()) RETURNING *")
+            .bind(payload.username)
+            .bind(hashed_password)
+            .fetch_one(&*users_database)
+            .await;
 
-    // Add user
-    let new_user = sqlx::query_as::<_, User>("INSERT INTO users (username, hashed_password, last_login_at) VALUES ($1, $2, now()) RETURNING *")
-        .bind(payload.username)
-        .bind(hashed_password)
-        .fetch_one(&*users_database)
-        .await;
-    
-    // Return a new user or error
-    match new_user {
-        Ok(user) => (StatusCode::CREATED, Json(user)).into_response(),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Json(format!("{err}"))).into_response()
+        // Return a new user or error
+        match new_user {
+            Ok(user) => (StatusCode::CREATED, Json(user)).into_response(),
+            Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Json(format!("{err}"))).into_response()
+        }
+    }
+    else {
+        (StatusCode::BAD_REQUEST, Json("Username is less that 3 characters or password is less than 8!")).into_response()
     }
 }
 
